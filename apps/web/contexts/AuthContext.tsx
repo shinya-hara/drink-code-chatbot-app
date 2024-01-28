c38dfbe854d2cd44ac592840cb54bd8427a4e202
kou-yoshida <kouyoshida28@gmail.com>
2024-01-29 08:14:36 +0900

    🚧 セッションプロバイダーの実装

---
+++ apps/web/contexts/AuthContext.tsx
@@ -0,0 +1,23 @@
+import { createClient } from "@/utils/supabase/client";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext<any>(null); // null or session
+
+export const AuthProvider = ({ children }: any) => {
+  const [currentSession, setCurrentSession] = useState<any>(null); // TODO: 型定義
+  const supabase = createClient();
+
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { session: _session },
+      } = await supabase.auth.getSession();
+      setCurrentSession(_session);
+    })();
+  }, []);
+  return (
+    <AuthContext.Provider value={currentSession}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
