ab5ca5c3d786044f5bdac93567f0e728d7ade9c2
Shinya Hara <s.hara@sonicmoov.com>
2024-01-29 09:40:35 +0900

    🚧 Add route for posting messages

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
+})
+
+interface MessageRequest {
+  message: string
+}
+
+app.post('/messages', (req: Request<{}, {}, MessageRequest>, res: Response) => {
+  const message = req.body.message
+  res.status(200).json({ message })
 })
 
 app.listen(port, () => {
