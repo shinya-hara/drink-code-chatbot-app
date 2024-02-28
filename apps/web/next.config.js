401dfd7c433a276e23cd60c6ddad603f1c481e45
kou-yoshida <kouyoshida28@gmail.com>
2024-02-29 08:36:43 +0900

    🚧 フロント側からとりあえずapiを叩けるように

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return process.env.NODE_ENV === "development"
+      ? [
+          {
+            source: "/api/:path*",
+            destination: "http://localhost:3001/:path*",
+          },
+        ]
+      : [];
+  },
+};
 
 module.exports = nextConfig;
