9a5258450996411bd1f522b266eb6698dddf3825
Shinya Hara <s.hara@sonicmoov.com>
2024-03-28 09:50:31 +0900

    ✅ inMemoryRepositoryを作ってテストコードをリファクタリング

---
+++ apps/backend/src/repositories/inMemory/UserRepositoryImpl.ts
@@ -0,0 +1,24 @@
+import { User } from '@/domains/entities/User';
+import { UserRepository } from '@/domains/repositories/UserRepository';
+
+export class UserRepositoryImpl implements UserRepository {
+  constructor(private _users: User[] = []) {}
+
+  async findUniqueById(id: string): Promise<User | void> {
+    const user = this._users.find((user) => user.id === id);
+
+    if (!user) return;
+
+    return User.reconstruct({
+      id: user.id,
+      type: user.type,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
+  }
+
+  async create(user: User): Promise<User> {
+    this._users.push(user);
+    return user;
+  }
+}
--- apps/backend/src/usecases/resolveUserUseCase.spec.ts
+++ apps/backend/src/usecases/resolveUserUseCase.spec.ts
@@ -1,60 +1,47 @@
-import { PrismaClient } from '@prisma/client';
 import { ResolveUserUseCase } from './resolveUserUseCase';
-import { UserRepositoryImpl } from '../repositories/UserRepositoryImpl';
+import { UserRepositoryImpl } from '../repositories/inMemory/UserRepositoryImpl';
 import { User } from '../domains/entities/User';
 
-describe('resolveUserUseCase', () => {
+describe('resolveUserUseCaseV2', () => {
   it('ユーザーが見つかれば、そのユーザーを返す', async () => {
     // given
-    const existedUser = User.create({
-      id: '1',
-      type: 'USER',
-    });
-
-    const mockPrisma = {
-      user: {
-        findUnique: jest.fn().mockResolvedValue(existedUser),
-        create: jest.fn().mockResolvedValue(
-          User.create({
-            id: '2',
-            type: 'USER',
-          }),
-        ),
-      },
-    };
-
-    const useCase = new ResolveUserUseCase(
-      new UserRepositoryImpl(mockPrisma as unknown as PrismaClient),
-    );
+    const users = [
+      User.create({
+        id: '1',
+        type: 'USER',
+      }),
+    ];
+    const repository = new UserRepositoryImpl(users);
+    const spy = jest.spyOn(repository, 'create');
+    const useCase = new ResolveUserUseCase(repository);
 
     // when
-    const user = await useCase.execute('userId');
+    const user = await useCase.execute('1');
 
     // then
-    expect(user).toEqual(existedUser);
+    expect(spy).not.toHaveBeenCalled();
+    expect(user.id).toBe('1');
+    expect(user.type).toBe('USER');
   });
+
   it('ユーザーが見つからなければ、新たに作成されたユーザーを返す', async () => {
     // given
-    const createdUser = User.create({
-      id: '2',
-      type: 'USER',
-    });
-
-    const mockPrisma = {
-      user: {
-        findUnique: jest.fn().mockResolvedValue(undefined),
-        create: jest.fn().mockResolvedValue(createdUser),
-      },
-    };
-
-    const useCase = new ResolveUserUseCase(
-      new UserRepositoryImpl(mockPrisma as unknown as PrismaClient),
-    );
+    const users = [
+      User.create({
+        id: '1',
+        type: 'USER',
+      }),
+    ];
+    const repository = new UserRepositoryImpl(users);
+    const spy = jest.spyOn(repository, 'create');
+    const useCase = new ResolveUserUseCase(repository);
 
     // when
-    const user = await useCase.execute('userId');
+    const user = await useCase.execute('2');
 
     // then
-    expect(user).toEqual(createdUser);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(user.id).toBe('2');
+    expect(user.type).toBe('USER');
   });
 });
